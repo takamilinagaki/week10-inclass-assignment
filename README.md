2c05fd5893e4687c5f4140da1592b9f991f579e4
TakamiInagaki <takami.l.inagaki@gmail.com>
2023-03-06 10:58:22 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Language Translation
+
+# Languages
+- English
+- French
+
+# Installation
+'''
+npm install next-i18next
+'''
+
+'''
+npm i react-intl
+
+
+# Description
+week10 in class assignment
+
+# Command lines
+- npx-create-next-app
+* npm run dev
+
+# Tools
+- next.js
+* javascript
++ json
+
+
---
+++ pages/index.js
@@ -0,0 +1,54 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
+import { FormattedMessage, useIntl } from 'react-intl'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+
+
+export default function Home({dir}) {
+
+const { locales } = useRouter();
+const intl = useIntl();
+
+const title = intl.formatMessage({ id: "page.home.head.title"})
+const description = intl.formatMessage ({id: "page.home.head.meta.description"})
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" hrefLang='x-default'/>
+        <link rel="icon" href="/favicon.ico" hrefLang='en'/>
+        <link rel="icon" href="/favicon.ico" hrefLang='fr'/>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+
+       
+
+        <header>
+        <div>
+       {[...locales].sort().map((locale)=>(
+        <Link key={locale} href="/" locale = {locale}>
+          <div>{locale}</div>
+          </Link>
+       ))}
+        </div>
+      </header>
+<main dir={dir} className={styles.main} >
+  <h1>
+    <FormattedMessage id="page.home.title" values={{b: (info) => <b>{info}</b>}}/>
+  </h1>
+  <p className={styles.description}>
+    <FormattedMessage id="page.home.description"/>
+  </p>
+</main>
+
+ 
+    </>
+  )
+}
